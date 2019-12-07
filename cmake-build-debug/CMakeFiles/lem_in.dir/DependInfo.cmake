786f73a3855c7a06ca0d9b3d554da351254f21f6
anisa121 <anisa56695@gmail.com>
2019-12-07 18:16:40 +0300

    f

---
+++ cmake-build-debug/CMakeFiles/lem_in.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/sdoughnu/Desktop/lem_in/algo.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/algo.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_atoi.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_atoi.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_bzero.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_bzero.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_isalnum.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_isalnum.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_isalpha.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_isalpha.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_isascii.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_isascii.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_isdigit.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_isdigit.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_isprint.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_isprint.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_itoa.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_itoa.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_lstadd.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_lstadd.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_lstdel.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_lstdel.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_lstdelone.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_lstdelone.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_lstiter.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_lstiter.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_lstmap.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_lstmap.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_lstnew.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_lstnew.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memalloc.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memalloc.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memccpy.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memccpy.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memchr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memchr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memcmp.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memcmp.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memcpy.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memcpy.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memdel.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memdel.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memmove.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memmove.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_memset.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_memset.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putchar.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putchar.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putchar_fd.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putchar_fd.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putendl.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putendl.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putendl_fd.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putendl_fd.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putnbr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putnbr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putnbr_fd.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putnbr_fd.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putstr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putstr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_putstr_fd.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_putstr_fd.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_str_is_alpha.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_str_is_alpha.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_str_is_numeric.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_str_is_numeric.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strcapitalize.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strcapitalize.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strcat.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strcat.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strchr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strchr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strclr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strclr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strcmp.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strcmp.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strcpy.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strcpy.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strdel.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strdel.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strdup.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strdup.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strequ.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strequ.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_striter.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_striter.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_striteri.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_striteri.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strjoin.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strjoin.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strlcat.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strlcat.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strlen.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strlen.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strlowcase.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strlowcase.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strmap.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strmap.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strmapi.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strmapi.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strncat.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strncat.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strncmp.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strncmp.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strncpy.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strncpy.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strnequ.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strnequ.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strnew.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strnew.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strnstr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strnstr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strrchr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strrchr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strsplit.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strsplit.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strstr.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strstr.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strsub.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strsub.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strtrim.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strtrim.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_strupcase.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_strupcase.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_tolower.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_tolower.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/libft/ft_toupper.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/libft/ft_toupper.c.o"
+  "/Users/sdoughnu/Desktop/lem_in/main.c" "/Users/sdoughnu/Desktop/lem_in/cmake-build-debug/CMakeFiles/lem_in.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../libft"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
